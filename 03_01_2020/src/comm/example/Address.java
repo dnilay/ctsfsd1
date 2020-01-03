49cbdde44c270ac0fe0e62308c4ca844f1013122
Nilay <mailnilaydas@gmail.com>
2020-01-03 17:21:15 +0530

    Added toString to Address

--- 03_01_2020/src/comm/example/Address.java
+++ 03_01_2020/src/comm/example/Address.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
+@ToString
 public class Address {
 	private String addressLine1;
 	private String addressLine2;
