25eb86b0a70c99f3744aa98a014cc15a082a1203
Nilay <mailnilaydas@gmail.com>
2020-01-06 10:50:26 +0530

    added arraycopy

--- 06_01_20/src/comm/example/Tester.java
+++ 06_01_20/src/comm/example/Tester.java
@@ -7,8 +7,21 @@ public class Tester {
 		/*
 		 * myArray.createArray(); myArray.displayArray();
 		 */
-	myArray.convertStringToArray("Hello World");
-	myArray.displayConvertedArray();
+	//myArray.convertStringToArray("Hello World");
+	//myArray.displayConvertedArray();
+	//original array
+	int[] myArray1 = { 1, 2, 3, 4, 5, 6 };
+	
+	// new larger array
+	int[] hold = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+	
+	 // copy all of the myArray array to the hold
+	 // array, starting with the 0th index
+	 System.arraycopy(myArray1, 0, hold, 0, myArray1.length);
+	for(int a:hold)
+	{
+		System.out.println(a);
+	}
 	}
 
 }
