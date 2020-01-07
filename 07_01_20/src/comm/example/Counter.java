7e81f02826bb89dafd385ebd26f09b38e8e9766f
Nilay <mailnilaydas@gmail.com>
2020-01-07 14:53:09 +0530

    final

--- 07_01_20/src/comm/example/Counter.java
+++ 07_01_20/src/comm/example/Counter.java
@@ -4,6 +4,23 @@ public class Counter {
 
 	private static Integer count;
 	private String temp;
+	//blank final variable
+	private final int MAX;
+	
+	
+	class InnerCounter{
+		public void add(int a,int b)
+		{
+			System.out.println("\n"+(a+b));
+		}
+	}
+	
+	public Counter() {
+		super();
+		MAX=1000;
+	}
+	
+
 	//static block is used to initialize static attributes
 	static {
 		count = 0;
@@ -11,6 +28,7 @@ public class Counter {
 
 	public Counter(String temp) {
 		super();
+		MAX=100;
 		count++;
 		this.temp = temp;
 	}
@@ -22,6 +40,11 @@ public class Counter {
 
 	public static String displayCount() {
 		return count.toString();
+	}
+	
+	public void displayMAX()
+	{
+		System.out.println(MAX);
 	}
 
 }
--- 07_01_20/src/comm/example/Tester.java
+++ 07_01_20/src/comm/example/Tester.java
@@ -3,14 +3,18 @@ package comm.example;
 public class Tester {
 
 	public static void main(String[] args) {
-		Counter counter1=new Counter("MyObject-1== ");
-		System.out.printf("%s",counter1);
-		Counter counter2=new Counter("MyObject-2== ");
-		System.out.printf("%s",counter2.toString());
-		Counter counter3=new Counter("MyObject-3== ");
-		System.out.printf("%s",counter3.toString());
-		Counter counter4=new Counter("MyObject-4== ");
-		System.out.printf("%s",Counter.displayCount());
+		/*
+		 * Counter counter1 = new Counter("MyObject-1== "); System.out.printf("%s",
+		 * counter1); Counter counter2 = new Counter("MyObject-2== ");
+		 * System.out.printf("%s", counter2.toString()); Counter counter3 = new
+		 * Counter("MyObject-3== "); System.out.printf("%s", counter3.toString());
+		 * Counter counter4 = new Counter("MyObject-4== "); System.out.printf("%s",
+		 * Counter.displayCount());
+		 */
+		Counter c = new Counter();
+		Counter.InnerCounter iCounter = c.new InnerCounter();
+		iCounter.add(10, 20);
+		c.displayMAX();
 
 	}
 
