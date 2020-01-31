237c3aa9969e9f9757fb38a262fb1bb789c61bb8
Nilay <mailnilaydas@gmail.com>
2020-01-31 12:19:55 +0530

    commit

---
+++ form-controls/src/comm/example/spring/config/DemoAppConfig.java
@@ -0,0 +1,36 @@
+package comm.example.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="comm.example.spring")
+public class DemoAppConfig {
+
+
+	@Bean
+	public ViewResolver viewResolver() {
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
