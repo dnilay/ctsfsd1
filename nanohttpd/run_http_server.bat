459252c790b9ae3c5ab95759664248a39701ea82
Nilay <54245048+dnilay@users.noreply.github.com>
2020-02-07 10:35:10 +0530

    added nanohttpd

---
+++ nanohttpd/run_http_server.bat
@@ -0,0 +1,1 @@
+java -classpath nanohttpd/nanohttpd-2.0.3.jar;nanohttpd/nanohttpd-webserver-2.0.3.jar fi.iki.elonen.SimpleWebServer --port 8000
---
+++ nanohttpd/run_http_server.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -classpath nanohttpd/nanohttpd-2.0.3.jar:nanohttpd/nanohttpd-webserver-2.0.3.jar fi.iki.elonen.SimpleWebServer --port 8000
