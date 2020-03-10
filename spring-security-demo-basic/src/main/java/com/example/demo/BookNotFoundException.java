4d168feec39ff5b45e85ae23ed96d103126942cf
mailnilaydas@gmail.com <mailnilaydas@gmail.com>
2020-03-10 11:10:02 +0530

    commit

---
+++ spring-security-demo-basic/src/main/java/com/example/demo/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(Long id) {
+        super("Book id not found : " + id);
+    }
+
+}
